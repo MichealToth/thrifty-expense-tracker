32f9fdb69f16899aa269dc4c07696f062cc7a185
MichealLaszloT-EU <kappamikey888@gmail.com>
2020-09-16 13:40:01 +0200

    Post NPM run Build / Pre Heroku deploy

---
+++ client/build/precache-manifest.d3574ef5bab8e3e8ee8e5615ee7f4587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd82a6b7dc2052b15c7d518a36e2268f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a199591d039c338fcfd",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a199591d039c338fcfd",
+    "url": "/static/js/main.e44185ce.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
